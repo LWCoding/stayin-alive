3fc572d9a7304fcfcc311109adbd6ce167f33b08
LWCoding <lwangprivate@gmail.com>
2025-11-22 01:44:55 -0800

    Added popup to recommend player to first play the tutorial

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -7,6 +7,11 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class TitleManager : MonoBehaviour
 {
+    /// <summary>
+    /// Static variable to track if the player has been prompted with the tutorial popup in this session.
+    /// </summary>
+    private static bool _hasBeenPromptedWithPopup = false;
+    
     [Header("Animation Settings")]
     [Tooltip("The Animator component that will play the 'Start' animation.")]
     [SerializeField] private Animator _animator;
@@ -19,11 +24,52 @@ public class TitleManager : MonoBehaviour
     [Tooltip("The GameObject to show during the animation to block UI interactions. Should have a GraphicsRaycaster component.")]
     [SerializeField] private GameObject _uiBlocker;
     
+    [Header("Tutorial Popup")]
+    [Tooltip("The popup GameObject to show if the player hasn't played the tutorial yet. Should be inactive by default.")]
+    [SerializeField] private GameObject _tutorialPopup;
+    
+    private void Awake()
+    {
+        // Hide the tutorial popup by default
+        if (_tutorialPopup != null)
+        {
+            _tutorialPopup.SetActive(false);
+        }
+    }
+    
+    /// <summary>
+    /// Hides the tutorial popup. Can be called from a button to close the popup.
+    /// </summary>
+    public void HideTutorialPopup()
+    {
+        if (_tutorialPopup != null)
+        {
+            _tutorialPopup.SetActive(false);
+        }
+    }
+    
     /// <summary>
     /// Plays the "Start" animation and then switches to the next scene when the animation is complete.
+    /// If the player hasn't been prompted with the popup before, shows a popup instead.
     /// </summary>
     public void Play()
     {
+        // Check if player has been prompted with popup before
+        if (!_hasBeenPromptedWithPopup)
+        {
+            // Show popup and immediately mark as prompted
+            _hasBeenPromptedWithPopup = true;
+            if (_tutorialPopup != null)
+            {
+                _tutorialPopup.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning("TitleManager: Tutorial popup is not assigned!");
+            }
+            return;
+        }
+        
         if (_animator == null)
         {
             Debug.LogError("TitleManager: Animator is not assigned!");
