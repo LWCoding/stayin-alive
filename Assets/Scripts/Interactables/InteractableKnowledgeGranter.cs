d16830e46ca29b268948ce5eadd6948f12fab0b1
Leyth Toubassy <lizardman1028@gmail.com>
2025-12-06 17:33:11 -0800

    script for adding thingy

---
+++ Assets/Scripts/Interactables/InteractableKnowledgeGranter.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableKnowledgeGranter : MonoBehaviour {
+  private Interactable interactable;
+
+  // private void OnTriggerEnter2D(Collider2D other) {
+  //   if (other.gameObject.tag == "Player") {
+  //     
+  //   }
+  // }
+}
